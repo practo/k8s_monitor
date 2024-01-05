b360b7f06b6434d92197493b465c963d14020a00
roy.a2yush <roy.a2yush@gmail.com>
2024-01-05 18:05:56 +0530

    automated the entire process

--- create_action_plan.py
+++ create_action_plan.py
@@ -3,51 +3,57 @@ from kubernetes import client, config
 import csv
 from get_details_for_kuber_cluster import convert_memory_to_ki, convert_cpu_to_millicore
 import pandas as pd
+from datetime import datetime
+import logging
 
-file_path = "k8s_output.csv"
+def create_action_plan(k8s_view_file, timestamp):
+    # Read the CSV file into a pandas DataFrame
+    data = pd.read_csv(k8s_view_file)
 
-# Read the CSV file into a pandas DataFrame
-data = pd.read_csv(file_path)
+    total_node_allocated_cpu = 0
+    total_node_allocated_memory = 0
+    total_remaining_node_request_cpu = 0
+    total_remaining_node_request_memory = 0
+    total_remaining_node_limit_cpu = 0
+    total_remaining_node_limit_memory = 0
+    k8s_descending_data = []
 
-total_node_allocated_cpu = 0
-total_node_allocated_memory = 0
-total_remaining_node_request_cpu = 0
-total_remaining_node_request_memory = 0
-total_remaining_node_limit_cpu = 0
-total_remaining_node_limit_memory = 0
-k8s_descending_data = []
+    for index, row in data.iterrows():
+        if row['view'] == 'NODE_VIEW':
+            total_node_allocated_cpu += convert_cpu_to_millicore(row['allocated_cpu'])
+            total_node_allocated_memory += convert_memory_to_ki(row['allocated_memory'])
+            total_remaining_node_request_cpu += convert_cpu_to_millicore(row['remaining_cpu_request'])
+            total_remaining_node_request_memory += convert_memory_to_ki(row['remaining_memory_request'])
+            total_remaining_node_limit_cpu += convert_cpu_to_millicore(row['remaining_cpu_limit'])
+            total_remaining_node_limit_memory += convert_memory_to_ki(row['remaining_memory_limit'])
+            row['remaining_cpu_request_percentage'] = round(float(float(convert_cpu_to_millicore(row['remaining_cpu_request'])) / float(convert_cpu_to_millicore(row['allocated_cpu']))) * 100)
+            row['remaining_memory_request_percentage'] = round(float(float(convert_memory_to_ki(row['remaining_memory_request'])) / float(convert_memory_to_ki(row['allocated_memory']))) * 100)
+            row['remaining_cpu_limit_percentage'] = round(float(float(convert_cpu_to_millicore(row['remaining_cpu_limit'])) / float(convert_cpu_to_millicore(row['allocated_cpu']))) * 100)
+            row['remaining_memory_limit_percentage'] = round(float(float(convert_memory_to_ki(row['remaining_memory_limit'])) / float(convert_memory_to_ki(row['allocated_memory']))) * 100)
+            k8s_descending_data.append(row)
 
-for index, row in data.iterrows():
-    if row['view'] == 'NODE_VIEW':
-        total_node_allocated_cpu += convert_cpu_to_millicore(row['allocated_cpu'])
-        total_node_allocated_memory += convert_memory_to_ki(row['allocated_memory'])
-        total_remaining_node_request_cpu += convert_cpu_to_millicore(row['remaining_cpu_request'])
-        total_remaining_node_request_memory += convert_memory_to_ki(row['remaining_memory_request'])
-        total_remaining_node_limit_cpu += convert_cpu_to_millicore(row['remaining_cpu_limit'])
-        total_remaining_node_limit_memory += convert_memory_to_ki(row['remaining_memory_limit'])
-        row['remaining_cpu_request_percentage'] = round(float(float(convert_cpu_to_millicore(row['remaining_cpu_request'])) / float(convert_cpu_to_millicore(row['allocated_cpu']))) * 100)
-        row['remaining_memory_request_percentage'] = round(float(float(convert_memory_to_ki(row['remaining_memory_request'])) / float(convert_memory_to_ki(row['allocated_memory']))) * 100)
-        row['remaining_cpu_limit_percentage'] = round(float(float(convert_cpu_to_millicore(row['remaining_cpu_limit'])) / float(convert_cpu_to_millicore(row['allocated_cpu']))) * 100)
-        row['remaining_memory_limit_percentage'] = round(float(float(convert_memory_to_ki(row['remaining_memory_limit'])) / float(convert_memory_to_ki(row['allocated_memory']))) * 100)
-        k8s_descending_data.append(row)
+    sorted_k8s_data = sorted(k8s_descending_data, key=lambda x: (
+        -x['remaining_cpu_request_percentage'],
+        -x['remaining_memory_request_percentage'],
+        -x['remaining_cpu_limit_percentage'],
+        -x['remaining_memory_limit_percentage']
+    ))
 
-sorted_k8s_data = sorted(k8s_descending_data, key=lambda x: (
-    -x['remaining_cpu_request_percentage'],
-    -x['remaining_memory_request_percentage'],
-    -x['remaining_cpu_limit_percentage'],
-    -x['remaining_memory_limit_percentage']
-))
+    logging.debug("\nSorted:", sorted_k8s_data)
 
-print("\nSorted:", sorted_k8s_data)
+    csv_file_path = f'k8s_action_plan_{timestamp}.csv'
 
-csv_file_path = 'k8s_descending.csv'
+    df = pd.DataFrame(sorted_k8s_data)
+    df.to_csv(csv_file_path, index=False)
 
-df = pd.DataFrame(sorted_k8s_data)
-df.to_csv(csv_file_path, index=False)
+    logging.debug(f"Total remaining node allocated cpu: {total_node_allocated_cpu}")
+    logging.debug(f"Total remaining node allocated memory: {total_node_allocated_memory}")
+    logging.debug(f"Total remaining node request cpu: {total_remaining_node_request_cpu}")
+    logging.debug(f"Total remaining node request memory: {total_remaining_node_request_memory}")
+    logging.debug(f"Total remaining node limit cpu: {total_remaining_node_limit_cpu}")
+    logging.debug(f"Total remaining node limit memory: {total_remaining_node_limit_memory}")
 
-print(f"Total remaining node allocated cpu: {total_node_allocated_cpu}")
-print(f"Total remaining node allocated memory: {total_node_allocated_memory}")
-print(f"Total remaining node request cpu: {total_remaining_node_request_cpu}")
-print(f"Total remaining node request memory: {total_remaining_node_request_memory}")
-print(f"Total remaining node limit cpu: {total_remaining_node_limit_cpu}")
-print(f"Total remaining node limit memory: {total_remaining_node_limit_memory}")
+if __name__ == "__main__":
+    now = datetime.now()
+    timestamp = now.strftime("%d_%m_%Y_%H_%M_%S")
+    create_action_plan()
--- drain_nodes.py
+++ drain_nodes.py
@@ -4,62 +4,34 @@ from datetime import datetime
 import argparse
 import time
 from kubernetes.client import V1DeleteOptions
-# from kubernetes.client.models import V1beta1Eviction
+import logging
 
-def run(cluster, node):
-    # Load the Kubernetes configuration
-    config.load_kube_config(context=cluster)
+def drain_node(cluster, node):
+  # Load the Kubernetes configuration
+  config.load_kube_config(context=cluster)
 
-    # Create a Kubernetes API client
-    api_client = client.CoreV1Api()
+  # Create a Kubernetes API client
+  api_client = client.CoreV1Api()
 
-    file_path = "k8s_descending.csv"
+  file_path = "k8s_descending.csv"
 
-    # Read the CSV file into a pandas DataFrame
-    data = pd.read_csv(file_path)
+  # Read the CSV file into a pandas DataFrame
+  data = pd.read_csv(file_path)
 
-    # Select the row where view = 'NODE_VIEW' and node_name is the variable node
-    selected_row = data[(data['view'] == 'NODE_VIEW') & (data['node_name'] == node)]
+  # Select the row where view = 'NODE_VIEW' and node_name is the variable node
+  selected_row = data[(data['view'] == 'NODE_VIEW') & (data['node_name'] == node)]
 
-    # Iterate through the selected row
-    for index, row in selected_row.iterrows():
-        node_name = row['node_name']
-        print(f"Draining Node-Name: {node_name}")
-        drain_node(api_client, node_name)
+  # Check if the selected row is empty
+  if selected_row.empty:
+    logging.error(f"No node with name '{node}' found in the CSV file.")
+  else:
+    logging.info(f"Draining Node-Name: {node}")
+    drain_and_delete_node(api_client, node)
+    logging.info(f"Node {node} drained successfully.")
 
-
-######################################################################################################################################
-
-
-
-def drain_node(api_client, node_name, grace_period = -1, ignore_daemonsets = False):
+def drain_and_delete_node(api_client, node_name, grace_period = -1, ignore_daemonsets = False):
     # Set the node unschedulable
     api_client.patch_node(node_name, {"spec": {"unschedulable": True}})
-    # Evict all pods from the node
-    # api_client.delete_node(node_name)
-    # pods = api_client.list_pod_for_all_namespaces(field_selector=f'spec.nodeName={node_name}')
-
-    # Evict pods gracefully, respecting PodDisruptionBudgets
-    # eviction = client.V1beta1Eviction(metadata={"name": node_name})
-    # if grace_period > 0:
-    #     eviction.delete_options = client.V1DeleteOptions(grace_period_seconds=grace_period)
-    # if ignore_daemonsets:
-    #     eviction.delete_options.propagation_policy = 'Background'  # Evict DaemonSet-managed pods
-
-    # api = client.AppsV1Api()
-    # while True:
-    #     try:
-    #         api.create_namespaced_pod_eviction(node_name, eviction)
-    #         break
-    #     except client.exceptions.ApiException as e:
-    #         if e.status == 429:  # Too many requests, retry with exponential backoff
-    #             time.sleep(2**retry_count)
-    #             retry_count += 1
-    #         else:
-    #             raise
-
-    # print(f"Node {node_name} drained successfully.")
-
     pods = api_client.list_pod_for_all_namespaces(field_selector=f'spec.nodeName={node_name}')
 
     # Delete all pods from the node
@@ -72,17 +44,15 @@ def drain_node(api_client, node_name, grace_period = -1, ignore_daemonsets = Fal
       )
     api_client.delete_node(node_name)
 
-
-######################################################################################################################################
-
 def parse_arguments():
   parser = argparse.ArgumentParser()
   parser.add_argument('-c', '--kubecontext', help='Specify the Kubernetes Cluster', required=True)
   parser.add_argument('-n', '--node', help='Specify the node name from the output file to drain', required=True)
+  parser.add_argument('-f', '--force_delete', help='Force drain the node', default='False')
 
   args = parser.parse_args()
-  return args.kubecontext, args.node
+  return args.kubecontext, args.node, args.force_delete
 
 if __name__ == "__main__":
-  cluster, node = parse_arguments()
-  run(cluster, node)
+  cluster, node, force_delete = parse_arguments()
+  drain_node(cluster, node, for)
