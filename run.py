3f0c953d6d71db79c57042aebf95ab6398993d1c
roy.a2yush <roy.a2yush@gmail.com>
2024-01-08 12:51:01 +0530

    added timestamp in logging

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     numeric_level = getattr(logging, log_level.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError(f'Invalid log level: {log_level}')
-    logging.basicConfig(level=numeric_level)
+    logging.basicConfig(level=numeric_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Now you can use logging.info(), logging.debug(), etc. throughout your code
     logging.debug("Running in Debug Mode")
