5ab09834ef355a36e2913294a88d92eb74bd9a4c
roy.a2yush <roy.a2yush@gmail.com>
2024-01-11 13:14:01 +0530

    added namespace names and count in output

--- print_number_of_nodes.py
+++ print_number_of_nodes.py
@@ -3,15 +3,26 @@ import argparse
 from kubernetes import client, config
 
 def print_number_of_nodes(cluster):
-  # Load the Kubernetes configuration
-  config.load_kube_config(context=cluster)
+    # Load the Kubernetes configuration
+    config.load_kube_config(context=cluster)
 
-  # Create a Kubernetes API client
-  api_client = client.CoreV1Api()
+    # Create a Kubernetes API client
+    api_client = client.CoreV1Api()
 
-  nodes = api_client.list_node(pretty=True)
-  logging.info(f"Total number of nodes: {len(nodes.items)}")
+    nodes = api_client.list_node(pretty=True)
+    logging.info(f"Total number of nodes: {len(nodes.items)}")
 
+def print_number_of_namespaces(cluster):
+    # Load the Kubernetes configuration
+    config.load_kube_config(context=cluster)
+
+    # Create a Kubernetes API client
+    api_client = client.CoreV1Api()
+
+    namespaces = api_client.list_namespace(pretty=True)
+    logging.info(f"Total number of namespaces: {len(namespaces.items)}")
+    namespace_names = [namespace.metadata.name for namespace in namespaces.items]
+    logging.info(f"Namespace names: {namespace_names}")
 
 def parse_arguments():
   parser = argparse.ArgumentParser()
@@ -23,3 +34,4 @@ if __name__ == "__main__":
   kubecontext = parse_arguments()
   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='logs/main.log')
   print_number_of_nodes(kubecontext)
+  print_number_of_namespaces(kubecontext)
